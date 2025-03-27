776390ceeac6954e281895db5ab92ef94ab705a9
Ryota Arai <ryota.arai@gmail.com>
2025-03-27 09:07:42 +0900

    refactor: Refactor client.go

--- cli/client/client.go
+++ cli/client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -11,6 +12,10 @@ import (
 	"connectrpc.com/connect"
 	hashiruv1 "github.com/ryotarai/hashiru/gen/hashiru/v1"
 	"github.com/ryotarai/hashiru/gen/hashiru/v1/hashiruv1connect"
+)
+
+const (
+	exitCodeForError = 125
 )
 
 // Run executes a command through the server and returns the exit code.
@@ -28,8 +33,8 @@ func Run(ctx context.Context, socketPath string, args []string) (int, error) {
 		"http://unix",
 	)
 
-	dir, err := os.Getwd()
-	if err != nil {
+		dir, err := os.Getwd()
+		if err != nil {
 		return 1, err
 	}
 
@@ -42,31 +47,39 @@ func Run(ctx context.Context, socketPath string, args []string) (int, error) {
 
 	stream, err := client.RunCommand(ctx, connect.NewRequest(req))
 	if err != nil {
-		return 1, err
+		return exitCodeForError, err
 	}
 
 	for stream.Receive() {
 		res := stream.Msg()
+		var base64Body string
+		var output io.Writer
 		switch res.Result.(type) {
 		case *hashiruv1.RunCommandResponse_StdoutBase64:
-			stdout, err := base64.StdEncoding.DecodeString(res.GetStdoutBase64())
-			if err != nil {
-				return 1, err
-			}
-			fmt.Fprint(os.Stdout, string(stdout))
+			base64Body = res.GetStdoutBase64()
+			output = os.Stdout
 		case *hashiruv1.RunCommandResponse_StderrBase64:
-			stderr, err := base64.StdEncoding.DecodeString(res.GetStderrBase64())
-			if err != nil {
-				return 1, err
-			}
-			fmt.Fprint(os.Stderr, string(stderr))
+			base64Body = res.GetStderrBase64()
+			output = os.Stderr
 		case *hashiruv1.RunCommandResponse_ExitCode:
 			return int(res.GetExitCode()), nil
+		default:
+			return exitCodeForError, fmt.Errorf("unknown response: %v", res)
+		}
+
+		if output != nil {
+			decoded, err := base64.StdEncoding.DecodeString(base64Body)
+			if err != nil {
+				return exitCodeForError, err
+			}
+			if _, err := output.Write(decoded); err != nil {
+				return exitCodeForError, err
+			}
 		}
 	}
 
 	if err := stream.Err(); err != nil {
-		return 1, err
+		return exitCodeForError, err
 	}
 
 	return 0, nil
